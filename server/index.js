00f5e0cbb809a9f69c66120a2515d581b1c514c1
Rudra Dev <rudratank07@gmail.com>
2024-10-04 22:54:54 +0530

    Creating Server And Basic Nodjs Setups

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import conn from "./connection.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5009;
+const databaseURL=process.env.DATABASE_URL;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PATCH","DELETE","PUT"],
+    credentials:true
+}))
+
+app.use(cookieParser());
+
+app.use(express.json());
+const server = app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+})
+
+conn(databaseURL);
