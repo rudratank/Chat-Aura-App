aa7a9debe3004321ebe74a46846da5ce29017984
Rudra Dev <rudratank07@gmail.com>
2024-10-04 22:22:38 +0530

    Making Auth Ui

--- client/src/pages/Auth/index.jsx
+++ client/src/pages/Auth/index.jsx
@@ -1,10 +1,45 @@
+//import React from 'react'
+import victory from "@/assets/victory.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 
 function Auth() {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw]xl:w-[60vw] rounded-3xl xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img src={victory} alt="victory emoji" className="h-[100px]" />
+            </div>
+            <p className="font-medium text-center">
+              Fill In the details to get started with the best chat app
+            </p>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Signup
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login"></TabsContent>
+              <TabsContent value="signup"></TabsContent>
+            </Tabs>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
