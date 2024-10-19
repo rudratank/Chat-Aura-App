3cb77a29ca239ecc70c5a08e552644c1acdbbd91
Rudra Dev <rudratank07@gmail.com>
2024-10-19 19:19:02 +0530

    Creating User Controller for UserModel

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,35 @@
+import user from "../models/UserModel.js";
+import {sign} from "jsonwebtoken"
+
+const maxAge = 3 * 24 * 60 * 60 * 1000; 
+
+const createToken = (email,userId) => {
+    return sign({email,userId},process.env.JWT_KEY,{ expiresIn:maxAge })
+};
+
+export const singup =async(request,respose,next)=>{
+    try{
+        const {email,pasword}=req.body;
+        if(!email || !pasword){
+            return respose.status(400).send("Email and Password is required...")
+        }
+        
+        const user= await user.create({email,pasword})
+        respose.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure:true,
+            sameSite:"None"
+        });
+
+        return respose.status(201).json({
+            user:{
+            id:user.id,
+            email:user.email,
+            profileSetup:user.profileSetup,
+        },
+    });
+    }catch({error}){
+        console.log({error});
+        return respose.status(500).send("Internal Server Error");
+    }
+}
