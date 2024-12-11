e3a17ce44b193c8d9608d0568cc228fc78215c52
rudratank <rudratank07@gmail.com>
2024-12-11 22:03:59 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
-const conn=(databaseurl)=>{
-
-    mongoose.connect(databaseurl)
-    .then(()=>console.log("Database Connection Successfull..."))
-    .catch(()=>console.log(err.message));
+const conn = (databaseurl) => {
+  mongoose.connect(databaseurl)
+    .then(() => console.log("Database Connection Successful..."))
+    .catch((err) => console.log("Database connection error:", err.message)); // Pass 'err' here
 };
 
 export default conn;
-
